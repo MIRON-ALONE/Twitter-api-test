7fd2f60a83fc1e7983fb06980bdc8cc732af04b6
MironaAloneh <mironaaloneh@gmail.com>
2025-03-01 18:33:33 +0600

    comlete local run without frontend

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import tweepy
 import os
+from fastapi import FastAPI, HTTPException
 
+app = FastAPI()
 
+client = tweepy.Client
 api_key = os.getenv('API_KEY')
 api_secret = os.getenv('API_SECRET')
 accses_token = os.getenv('ACCESS_TOKEN')
 accses_secret = os.getenv('ACCESS_SECRET')
+domain = "http://localhost:8000" # os.getenv('RAILWAY_PUBLIC_DOMAIN')
+def get_oauth_handler():
+    print(api_key)
+    print(api_secret)
+    print(accses_token)
+    print(accses_secret)
+    return tweepy.OAuth1UserHandler(consumer_key=api_key, consumer_secret=api_secret, callback=f"{domain}/auth/callback")
 
-auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(accses_token, accses_secret)
-
-api = tweepy.API(auth)
+oauth_sessions = {}
 
 
-def like_tweet(status_id):
+@app.get("/auth/login")
+async def login():
+    auth = get_oauth_handler()
     try:
-        api.create_favorite(status_id)
-        print(f"Complete: {status_id} ")
-    except tweepy.TweepyException as e:
-        print(f"Error: {e.response.status_code} - {e.response.text}")
+        auth_url = auth.get_authorization_url()
+        oauth_sessions[auth.request_token["oauth_token"]] = auth.request_token["oauth_token_secret"]
+        return {"auth_url": auth_url} 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ошибка авторизации: {str(e)}")
 
-status_id = "1878132133144715658"  
-api.create_favorite(status_id)
 
-print("Complete")
-   
+@app.get("/auth/callback")
+async def auth_callback(oauth_token: str, oauth_verifier: str):
+    auth = get_oauth_handler()
+    auth.request_token = {
+        "oauth_token": oauth_token,
+        "oauth_token_secret": oauth_sessions.pop(oauth_token),
+    }
+    access_token, access_token_secret = auth.get_access_token(oauth_verifier)
+    client = tweepy.Client(   
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret
+    )
+    
+    response = client.like(
+        tweet_id="1878132133144715658",
+        user_auth=True
+    )
+    print(f"-------------- access_token: {access_token}")
+    return response
